d4d14d883cd32c20d06b6deee6ccca02cefe8e06
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-13 00:54:58 -0300

    feat: create auth schema for login and register

--- src/schemas/auth/index.ts
+++ src/schemas/auth/index.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
-const loginSchema = z.object({
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+const LoginSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
 });
 
-export { loginSchema };
+const RegisterSchema = z.object({
+  fullName: z.string().min(1, { message: "FullName is required" }),
+  email: z.string().email({ message: "Email is required" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 character" }),
+});
+
+export { LoginSchema, RegisterSchema };
