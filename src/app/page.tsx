bcd1e94c487c470bc71e8074ae2aca07c1b7075e
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-13 00:49:34 -0300

    feat: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,41 @@
-import Header from "@/components/header";
-import LoginButton from "@/components/login-button";
+import Link from "next/link";
+
+import { Header } from "@/components/header";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+import { ChevronRight, Github } from "lucide-react";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <div className="flex min-h-screen flex-col items-center justify-center gap-3">
+      <div className="flex min-h-screen flex-col items-center justify-center gap-3 text-center px-10">
+        <Link
+          href="https://github.com/viniciuspra/nextjs-authjs-template"
+          target="_blank"
+        >
+          <Badge
+            variant={"secondary"}
+            className="flex items-center gap-3 py-1 rounded-full cursor-pointer border border-ring/20"
+          >
+            <Github size={16} />
+            <span>Check out this project on GitHub.</span>
+            <ChevronRight size={16} />
+          </Badge>
+        </Link>
+
         <h1 className="font-black text-4xl text-white">
           Authentication Template with Next.js
         </h1>
         <p className="text-lg text-white font-semibold">
           A ready-to-use authentication template built with Next.js and Auth.js.
         </p>
-        <LoginButton>
-          <Button className="mt-2 bg-darkShade text-primary border border-mediumShade hover:bg-darkShade/90">
+        <Button className="mt-2 bg-secondary text-primary hover:bg-secondary/90 p-0 shadow-shape">
+          <Link href="/auth/login" className="w-full h-full px-4 py-2">
             Get Started
-          </Button>
-        </LoginButton>
+          </Link>
+        </Button>
       </div>
     </main>
   );
