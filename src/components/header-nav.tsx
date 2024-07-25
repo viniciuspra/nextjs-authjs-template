e96a3e8a96b830491a14ec5f5da549be65b311e9
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-25 17:27:54 -0300

    style: move link styles to a variable for reuse

--- src/components/header-nav.tsx
+++ src/components/header-nav.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 interface NavProps {
@@ -7,36 +6,29 @@ interface NavProps {
 }
 
 export function HeaderNav({ mobile }: NavProps) {
+  const LinkStyle =
+    "inline-flex h-8 px-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-primary underline-offset-4 hover:underline";
   return (
     <nav
       className={`${
-        !mobile ? "md:flex items-center hidden" : "flex flex-col"
+        !mobile ? "lg:flex items-center hidden" : "flex flex-col"
       } w-full`}
     >
-      <Link
-        href="/auth/login"
-        className="inline-flex h-8 px-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-primary underline-offset-4 hover:underline"
-      >
+      <Link href="/auth/login" className={LinkStyle}>
         Login
       </Link>
 
-      <Link
-        href="/auth/register"
-        className="inline-flex h-8 px-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-primary underline-offset-4 hover:underline"
-      >
+      <Link href="/auth/register" className={LinkStyle}>
         Register
       </Link>
-      <Link
-        href="/auth/login"
-        className="inline-flex h-8 px-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-primary underline-offset-4 hover:underline"
-      >
+      <Link href="/auth/login" className={LinkStyle}>
         Two factor verification (2FA)
       </Link>
-      <Link
-        href="/auth/login"
-        className="inline-flex h-8 px-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors text-primary underline-offset-4 hover:underline"
-      >
+      <Link href="/auth/login" className={LinkStyle}>
         Forgot password
+      </Link>
+      <Link href="api/auth/providers" className={LinkStyle}>
+        Providers
       </Link>
     </nav>
   );
