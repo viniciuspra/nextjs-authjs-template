67c1a98dd28f615945e562e10e8c72aa275348ab
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-31 17:11:40 -0300

    feat: define ExtendedUser type with role and 2FA status

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,16 +2,13 @@ import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession, User } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+};
+
 declare module "next-auth" {
   interface Session {
-    user: {
-      role: UserRole;
-    } & DefaultSession["user"];
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    role: UserRole;
+    user: ExtendedUser;
   }
 }
