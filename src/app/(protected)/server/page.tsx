a2c9016fad106cf31cf38acf359dab4befc68b57
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-31 17:14:56 -0300

    feat: create server page

--- src/app/(protected)/server/page.tsx
+++ src/app/(protected)/server/page.tsx
@@ -1,13 +1,15 @@
+import { UserInfo } from "@/components/user-info";
 import { currentUser } from "@/lib/auth";
 
 export default async function ServerPage() {
   const user = await currentUser();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-5xl font-black">Server Example</h1>
-      <pre className="mt-2 rounded-md bg-muted p-4">
-        <code className="text-white">{JSON.stringify(user, null, 2)}</code>
-      </pre>
+      <UserInfo
+        label="Server Example"
+        description="This information comes from the server side."
+        user={user}
+      />
     </div>
   );
 }
