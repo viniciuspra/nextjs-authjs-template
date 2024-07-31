b9781bd277639f29c63e5f5c8e671e32da2ba169
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-31 20:03:54 -0300

    fix: add `isOAuth` field to ExtendedUser type in NextAuth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,10 +1,12 @@
-import { UserRole } from "@prisma/client";
-import NextAuth, { type DefaultSession, User } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { JWT } from "next-auth/jwt";
+
+import { UserRole } from "@prisma/client";
 
 export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
   isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
 };
 
 declare module "next-auth" {
