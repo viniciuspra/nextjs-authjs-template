cdb483191a7a5fe2fcb115c4adbedf88a01eca6d
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-31 20:07:18 -0300

    fix: add null check for user in useCurrentUser hook

--- src/hooks/use-current-user.ts
+++ src/hooks/use-current-user.ts
@@ -2,8 +2,10 @@ import { useSession } from "next-auth/react";
 
 export const useCurrentUser = () => {
   const session = useSession();
-  
-  const user = session.data?.user
+
+  const user = session.data?.user;
+
+  if (!user) return;
 
   return user;
-}
+};
