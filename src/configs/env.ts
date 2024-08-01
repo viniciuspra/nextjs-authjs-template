907c1ddf080b40b65ccc7c4a6b2967bc3ec4bfde
viniciuspra <vinicius.cascaesp@gmail.com>
2024-07-31 22:00:36 -0300

    refactor: rename APP_BASE_URL to BASE_URL

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 const envSchema = z.object({
   AUTH_SECRET: z.string(),
-  BASE_URL: z.string().url(),
-  DATABASE_URL: z.string().url(),
-  DATABASE_URL_UNPOOLED: z.string().url(),
+  NEXT_PUBLIC_APP_URL: z.string().url(),
+  DATANEXT_PUBLIC_APP_URL: z.string().url(),
+  DATANEXT_PUBLIC_APP_URL_UNPOOLED: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
